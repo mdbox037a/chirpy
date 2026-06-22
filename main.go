b033cf53b865c121dff2f706de9775ced100510f
Matt Box <mdbox037a@protonmail.com>
2026-06-22 14:01:38 -0400

    create middleware to wrap handlers in order to count number of site hits

--- main.go
+++ main.go
@@ -3,14 +3,29 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 const filepathRoot string = "."
 const port string = "8080"
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		log.Printf("%s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
+	apiCfg := apiConfig{}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("/healthz", handlerReadiness)
 
 	srv := &http.Server{
