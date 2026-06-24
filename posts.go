cb6eca6d36dfb06e382388c34b500a2108f2e033
Matt Box <mdbox037a@protonmail.com>
2026-06-23 20:31:56 -0400

    complete skeleton of handlerValidateChirp func

--- posts.go
+++ posts.go
@@ -1,7 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
-func handlerValidateChirp(wr http.ResponseWriter, r *http.Request) {
+func handlerValidateChirp(wr http.ResponseWriter, req *http.Request) {
+	type reqBody struct {
+		Body string `json:"body"`
+	}
 
+	decoder := json.NewDecoder(req.Body)
+	post := reqBody{}
+	err := decoder.Decode(&post)
+	if err != nil {
+		// TODO: call return error func
+		return
+	}
+
+	if len(post.Body) > 140 {
+		// TODO: call return error func
+	}
+
+	// TODO: call return json func
 }
