0db729e8da983f9071e5a9f7af15514f26d32b6b
Matt Box <mdbox037a@protonmail.com>
2026-07-25 07:50:40 -0400

    save refresh token query and regen sqlc code

---
+++ sql/queries/refresh_tokens.sql
@@ -0,0 +1,12 @@
+-- name: CreateRefreshToken :exec
+INSERT INTO refresh_tokens (
+    token, created_at, updated_at, user_id, expires_at, revoked_at
+)
+VALUES (
+    $1,
+    NOW(),
+    NOW(),
+    $2,
+    NOW() + INTERVAL '60 days',
+    NULL
+);
