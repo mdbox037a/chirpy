ea8e7e3fd679ce648980a2eb045108ec56000459
Matt Box <mdbox037a@protonmail.com>
2026-08-01 15:38:12 -0400

    modify delete chirp query to also check that user_id matches requesting user id

--- internal/database/chirps.sql.go
+++ internal/database/chirps.sql.go
@@ -12,13 +12,13 @@ import (
 )
 
 const createChirp = `-- name: CreateChirp :one
-INSERT INTO chirps(id, created_at, updated_at, body, user_id)
+INSERT INTO chirps (id, created_at, updated_at, body, user_id)
 VALUES (
-  gen_random_uuid(),
-  NOW(),
-  NOW(),
-  $1,
-  $2
+    gen_random_uuid(),
+    now(),
+    now(),
+    $1,
+    $2
 )
 RETURNING id, created_at, updated_at, body, user_id
 `
@@ -43,11 +43,16 @@ func (q *Queries) CreateChirp(ctx context.Context, arg CreateChirpParams) (Chirp
 
 const deleteChirp = `-- name: DeleteChirp :exec
 DELETE FROM chirps
-WHERE id = $1
+WHERE id = $1 AND user_id = $2
 `
 
-func (q *Queries) DeleteChirp(ctx context.Context, id uuid.UUID) error {
-	_, err := q.db.ExecContext(ctx, deleteChirp, id)
+type DeleteChirpParams struct {
+	ID     uuid.UUID
+	UserID uuid.UUID
+}
+
+func (q *Queries) DeleteChirp(ctx context.Context, arg DeleteChirpParams) error {
+	_, err := q.db.ExecContext(ctx, deleteChirp, arg.ID, arg.UserID)
 	return err
 }
 
--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -1,11 +1,11 @@
 -- name: CreateChirp :one
-INSERT INTO chirps(id, created_at, updated_at, body, user_id)
+INSERT INTO chirps (id, created_at, updated_at, body, user_id)
 VALUES (
-  gen_random_uuid(),
-  NOW(),
-  NOW(),
-  $1,
-  $2
+    gen_random_uuid(),
+    now(),
+    now(),
+    $1,
+    $2
 )
 RETURNING *;
 
@@ -22,4 +22,4 @@ WHERE id = $1;
 
 -- name: DeleteChirp :exec
 DELETE FROM chirps
-WHERE id = $1;
+WHERE id = $1 AND user_id = $2;
